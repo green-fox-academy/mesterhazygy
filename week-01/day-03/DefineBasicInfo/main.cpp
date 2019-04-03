0334f7f6683436b96c854ab33212cda5ecb41cc1
mesterhazygy <mesterhazygy@gmail.com>
2019-04-03 13:20:43 +0200

    solved definebasicinfo

---
+++ week-01/day-03/DefineBasicInfo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::string myname = "Mesterhazy Gyozo";
+    int age = 33;
+    double height = 1.66;
+    bool isMarried = true;
+
+    std::cout << myname << std::endl;
+    std::cout << age << std::endl;
+    std::cout << height << std::endl;
+    std::cout << isMarried << std::endl;
+
+    return 0;
+}
