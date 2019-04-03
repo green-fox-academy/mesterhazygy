dda3fd07fe54c297f15412ee5d3550cc66fd7188
mesterhazygy <mesterhazygy@gmail.com>
2019-04-03 17:27:33 +0200

    mostly solved conditionalvariablemutation

---
+++ week-01/day-03/ConditionalVariableMutation/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main() {
+    double a = 24;
+    int out = 0;
+    if ((int(a) % 2) == 0) {
+        std::cout << ++out << std::endl;
+    }
+
+    int b = 13;
+    std::string out2 = "";
+
+    if ((b > 10) && (b < 20)) {
+        out2 = "sweet";
+    } else if (b < 10) {
+        out2 = "less";
+    } else if (b > 20) {
+        out2 = "more";
+    }
+    std::cout << out2 << std::endl;
+
+    int c = 123;
+    int credits = 100;
+    bool isBonus = false;
+    if ((credits >= 50) && (isBonus == false)) {
+        std::cout << c - 2 << std::endl;
+    } else if ((credits < 50) && (isBonus = false)) {
+        std::cout << c - 1 << std::endl;
+    } else if (isBonus = true) {
+        std::cout << c << std::endl;
+    }
+
+    int d = 8;
+    int time = 120;
+    std::string out3 = "";
+
+
+
+    return 0;
+}
