fb0fc82a72ad448ef192b34bbba8ea79e933c057
mesterhazygy <mesterhazygy@gmail.com>
2019-04-04 16:04:50 +0200

    solved colors correctly

--- week-01/day-04/Colors/main.cpp
+++ week-01/day-04/Colors/main.cpp
@@ -2,19 +2,16 @@
 
 int main() {
 
-    // - Create a two dimensional array
-    //   which can contain the different shades of specified colors
-    // - In `colors[0]` store the shades of green:
-    //   `"lime", "forest green", "olive", "pale green", "spring green"`
-    // - In `colors[1]` store the shades of red:
-    //   `"orange red", "red", "tomato"`
-    // - In `colors[2]` store the shades of pink:
-    //   `"orchid", "violet", "pink", "hot pink"`
-
     std::string colors[5][5] = {{"lime", "forest green", "olive", "pale green", "spring green"},
-                                {"orange red", "red", "tomato", 0, 0},
-                                {"orchid", "violet", "pink", "hot pink", 0}
+                                {"orange red", "red", "tomato", "", ""},
+                                {"orchid", "violet", "pink", "hot pink", ""}
     };
+    // for (int i = 0; i < 5; i++) {
+    //    for (int j = 0; j < 5; j++) {
+    //        std::cout << colors [i][j] << std::endl;
+    //    }
+    //}
+
 
     return 0;
 }
