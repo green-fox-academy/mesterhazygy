3509c2ad13202e702c21650570ee9d35855408d1
mesterhazygy <mesterhazygy@gmail.com>
2019-04-04 16:37:04 +0200

    solved append_a

--- week-01/day-04/Append_A/main.cpp
+++ week-01/day-04/Append_A/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::string animals [] = {"koal", "pand", "zebr"};
+    for (int i = 0; i < 3; i++) {
+            animals[i] += "a";
+        std::cout << animals[i] << std::endl;
+    }
+
     return 0;
 }
