78625bd9bce7ae20fe9e16e4f2f9150b34a76015
mesterhazygy <mesterhazygy@gmail.com>
2019-04-04 18:03:58 +0200

    solved reverse

---
+++ week-01/day-04/Reverse/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void reverse(int[]);
+
+int main() {
+
+    int aj [] = {3, 4, 5, 6, 7};
+
+    int k = aj[0];
+    int l = aj[1];
+    aj[0] = aj[4];
+    aj[1] = aj[3];
+    aj[4] = k;
+    aj[3] = l;
+    std::cout << aj << std::endl;
+
+    return 0;
+}
