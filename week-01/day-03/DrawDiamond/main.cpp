1a9ceed3e2eaa32522e0a2db01d6b91397b37120
mesterhazygy <mesterhazygy@gmail.com>
2019-04-05 16:52:04 +0200

    solved drawdiamond yaaaaaay

--- week-01/day-03/DrawDiamond/main.cpp
+++ week-01/day-03/DrawDiamond/main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cout << "give me a number" << std::endl;
     std::cin >> a;
 
-    for (int i = 0; i < a; i ++) {
+    for (int i = 0; i < a / 2 + 1; i ++) {
         for (int j = 0; j < ((a / 2) - i) + 1; j ++) {
             std::cout << " ";
         }
@@ -14,6 +14,17 @@ int main() {
             std::cout << "*";
         }
         std::cout << std::endl;
+    }
+    for (int i = 0; i < a / 2; i ++) {          //sorok száma alul jó
+        for (int j = 0; j < i + 2; j ++) {      //space-ek száma jó
+            std::cout << " ";
+        }
+        for (int k = 0; k < a - (i + 1) * 2; k ++) {
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+
     }
 
 
