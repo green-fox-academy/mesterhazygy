6b04d42f57425e606d2835616a401f49a68b97d8
mesterhazygy <mesterhazygy@gmail.com>
2019-04-05 14:45:57 +0200

    solved pyramid

---
+++ week-01/day-03/DrawPyramid/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    int a;
+    std::cout << "give me a number" << std::endl;
+    std::cin >> a;
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < (a- (i + 1)); j++) {
+            std::cout << " ";
+        }
+        for (int k = 0; k < (2 * i) + 1; k++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
