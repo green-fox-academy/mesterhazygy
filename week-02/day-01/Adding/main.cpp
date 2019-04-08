586c73f232a85b1d5298b7bdffb11d0ced3f807d
mesterhazygy <mesterhazygy@gmail.com>
2019-04-08 13:16:20 +0200

    solved adding

---
+++ week-02/day-01/Adding/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int a = 20;
+    int b = 17;
+    int *aPointer = &a;
+    int *bPointer = &b;
+
+    int c = *aPointer + *bPointer;
+    std::cout << c << std::endl;
+
+    return 0;
+}
