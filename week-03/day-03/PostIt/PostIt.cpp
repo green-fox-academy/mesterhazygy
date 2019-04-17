1d902aef2da4a9122360bcca8d2cec4585b869e5
mesterhazygy <mesterhazygy@gmail.com>
2019-04-17 13:40:23 +0200

    solved postit with header and cpp files added

---
+++ week-03/day-03/PostIt/PostIt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by megha on 2019. 04. 17..
+//
+
+#include "PostIt.h"
---
+++ week-03/day-03/PostIt/PostIt.h
@@ -0,0 +1,14 @@
+//
+// Created by megha on 2019. 04. 17..
+//
+
+#ifndef POSTIT_POSTIT_H
+#define POSTIT_POSTIT_H
+
+
+class PostIt {
+
+};
+
+
+#endif //POSTIT_POSTIT_H
