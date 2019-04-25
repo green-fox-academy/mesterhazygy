d7a87cfb8d10641fa63c1034c0f8faec58fa731a
mesterhazygy <mesterhazygy@gmail.com>
2019-04-25 13:47:29 +0200

    tested sum function with zero and multiple elements and negative sum, works fine

--- week-04/day-04/Apples/Apples_tests/Apples_Test.cpp
+++ week-04/day-04/Apples/Apples_tests/Apples_Test.cpp
@@ -14,3 +14,24 @@ TEST(test_sum, test_summary){
     ASSERT_EQ(sum(myVector), 99);
 }
 
+TEST(test_sum, test_summaryWithZeroElements){
+
+    std::vector<int> myVector2 = {};
+
+    ASSERT_EQ(sum(myVector2), 0);
+}
+
+TEST(test_sum, test_summaryOneElement){
+
+    std::vector<int> myVector3 = {3};
+
+    ASSERT_EQ(sum(myVector3), 3);
+}
+
+TEST(test_sum, test_summaryIsNegative){
+
+    std::vector<int> myVector4 = {1, 3, -65, 7, 23};
+    bool isNegative = sum(myVector4) < 0;
+
+    ASSERT_EQ(isNegative, true);
+}
