90d26f3dba4a60116c9722dda8c44b8387e86539
mesterhazygy <mesterhazygy@gmail.com>
2019-05-20 16:12:20 +0200

    added bubblesort to linkedlist

--- week-06/day-05/LinkedList/main.c
+++ week-06/day-05/LinkedList/main.c
@@ -22,27 +22,41 @@ linked_list_t *delete(linked_list_t *head);
 
 linked_list_t *deleteFirst(linked_list_t *head);
 
+int deleteElements(linked_list_t *node, int value);
+
+void printElements(linked_list_t *node);
+
+linked_list_t *searchValue(linked_list_t *head, int value);
+
+linked_list_t *bubbleSort(linked_list_t *head);
+
+linked_list_t *insertionSort(linked_list_t *head);
+
 int main()
 {
     linked_list_t *head = (linked_list_t *) malloc(sizeof(linked_list_t));
     head->data = 0;
     head->next = NULL;
-    append(head, 1);
-    printf("%d\n", head->next->data);
+    append(head, 3);
+    //printf("%d\n", head->next->data);
     append(head, 2);
-    printf("%d\n", head->next->next->data);
+    append(head, 1);
+    //printf("%d\n", head->next->next->data);
     prepend1(head, -1);
-    printf("%d\n", head->next->data);
+    //printf("%d\n", head->next->data);
     head = prepend2(head, -2);
-    printf("%d\n", head->data);
+    //printf("%d\n", head->data);
     prepend3(&head, -3);
-    printf("%d\n", head->next->data);
-    printf("%d\n", size(head));
+    //printf("%d\n", head->next->data);
+    //printf("%d\n", size(head));
     head = delete(head);
-    printf("%d\n", size(head));
+    //printf("%d\n", size(head));
     head = deleteFirst(head);
-    printf("%d\n", size(head));
-
+    //printf("%d\n", size(head));
+    //printf("%d\n", deleteElements(head, 2));
+    //printElements(head);
+    head = bubbleSort(head);
+    printElements(head);
 
 
     return 0;
@@ -118,4 +132,67 @@ linked_list_t *deleteFirst(linked_list_t *head)
     head->next = head->next->next;
     free(temp);
     return head;
+}
+
+int deleteElements(linked_list_t *node, int value)
+{
+    int counter = 0;
+    while (node->next != NULL) {
+        if(node->next->data == value){
+            linked_list_t *temp = node->next;
+            node->next = node->next->next;
+            free(temp);
+            counter++;
+        }
+        node = node->next;
+    }
+    return counter;
+}
+
+void printElements(linked_list_t *node)
+{
+    int a = size(node);
+    for (int i = 0; i < a + 1; ++i) {
+        printf("%d, ", node->data);
+        node = node->next;
+    }
+}
+
+linked_list_t *searchValue(linked_list_t *node, int value)
+{
+    while(node->next != NULL){
+        if(node->data == value)
+            return node;
+        else
+            node = node->next;
+    }
+    return 0;
+}
+
+linked_list_t *bubbleSort(linked_list_t *node)
+{
+    int swapped = 1;
+    int a = size(node);
+    linked_list_t *head = node;
+    linked_list_t *temp = (linked_list_t *)malloc(sizeof(linked_list_t));
+    while(swapped){
+        swapped = 0;
+        while(node->next != NULL){
+            if(node->data > node->next->data){
+                swapped = 1;
+                temp->data = node->data;
+                node->data = node->next->data;
+                node->next->data = temp->data;
+            }
+            node = node->next;
+        }
+        node = head;
+    }
+    free(temp);
+    return head;
+}
+
+linked_list_t *insertionSort(linked_list_t *head)
+{
+    
 }
