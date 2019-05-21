3a823469c6710e54a239d682e60de5c5c0dac6f2
mesterhazygy <mesterhazygy@gmail.com>
2019-05-21 15:55:38 +0200

    solved FrequencyPlay with less loops

--- week-07/day-02/FrequencyPlay/main.c
+++ week-07/day-02/FrequencyPlay/main.c
@@ -18,63 +18,82 @@ int main(void) {
 	BSP_LED_Init(LED1);
 
 	while (1) {
-		int a = 0;
-		for (int i = 0; i < 12; i++) {
-			while (a < 3) {
+		/*
+		for (int i = 1; i < 9; i += i) {
+			for (int j = 0; j < 3; j++) {
 				BSP_LED_On(LED1);
-				HAL_Delay(1000);
+				HAL_Delay(i * 125);
 				BSP_LED_Off(LED1);
-				HAL_Delay(1000);
-				a++;
-			}
-			if (a == 3) {
-				while (a < 6) {
-					BSP_LED_On(LED1);
-					HAL_Delay(500);
-					BSP_LED_Off(LED1);
-					HAL_Delay(500);
-					a++;
-				}
-			}
-			if (a == 6) {
-				while (a < 9) {
-					BSP_LED_On(LED1);
-					HAL_Delay(250);
-					BSP_LED_Off(LED1);
-					HAL_Delay(250);
-					a++;
-				}
-			}
-			if (a == 9) {
-				while (a < 12) {
-					BSP_LED_On(LED1);
-					HAL_Delay(125);
-					BSP_LED_Off(LED1);
-					HAL_Delay(125);
-					a++;
-				}
-			}
-			if (a == 12) {
-				while (a < 15) {
-					BSP_LED_On(LED1);
-					HAL_Delay(250);
-					BSP_LED_Off(LED1);
-					HAL_Delay(250);
-					a++;
-				}
-			}
-			if (a == 15) {
-				while (a < 18) {
-					BSP_LED_On(LED1);
-					HAL_Delay(500);
-					BSP_LED_Off(LED1);
-					HAL_Delay(500);
-					a++;
-				}
-			}
-			if (a == 18){
-				a = 0;
+				HAL_Delay(i * 125);
 			}
 		}
+		for (int i = 4; i > 1; i = i / 2) {
+			for (int j = 0; j < 3; j++) {
+				BSP_LED_On(LED1);
+				HAL_Delay(i * 125);
+				BSP_LED_Off(LED1);
+				HAL_Delay(i * 125);
+			}
+		}
+		*/
+
+		 int a = 0;
+		 for (int i = 0; i < 12; i++) {
+		 while (a < 3) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(1000);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(1000);
+		 a++;
+		 }
+		 if (a == 3) {
+		 while (a < 6) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(500);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(500);
+		 a++;
+		 }
+		 }
+		 if (a == 6) {
+		 while (a < 9) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(250);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(250);
+		 a++;
+		 }
+		 }
+		 if (a == 9) {
+		 while (a < 12) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(125);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(125);
+		 a++;
+		 }
+		 }
+		 if (a == 12) {
+		 while (a < 15) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(250);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(250);
+		 a++;
+		 }
+		 }
+		 if (a == 15) {
+		 while (a < 18) {
+		 BSP_LED_On(LED1);
+		 HAL_Delay(500);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(500);
+		 a++;
+		 }
+		 }
+		 if (a == 18) {
+		 a = 0;
+		 }
+		 }
 	}
 }
