66b0d0e4ba6e29eac8b4c4f8ad75e8489d4d9290
mesterhazygy <mesterhazygy@gmail.com>
2019-05-22 15:13:32 +0200

    solved knightrider direction change

---
+++ week-07/day-03/KnightRiderDirChange/main.c
@@ -0,0 +1,101 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+void init_pins() {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOIEN; /* GPIOI clock enable */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOFEN; /* GPIOI clock enable */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+	// Init user led (I 1)
+	GPIOF->MODER |= (GPIO_MODER_MODER10_0); /* configure PI1 in output mode */
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_10); /* ensure push pull mode selected default */
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR10); /* ensure maximum speed setting (even though it is unnecessary) */
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR10); /* ensure all pull up pull down resistors are disabled */
+
+	GPIOF->MODER |= (GPIO_MODER_MODER9_0);
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_9);
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR9);
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR9);
+
+	GPIOF->MODER |= (GPIO_MODER_MODER8_0);
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_8);
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR8);
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR8);
+
+	GPIOF->MODER |= (GPIO_MODER_MODER7_0);
+	GPIOF->OTYPER &= ~(GPIO_OTYPER_OT_7);
+	GPIOF->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR7);
+	GPIOF->PUPDR &= ~(GPIO_PUPDR_PUPDR7);
+
+	GPIOB->MODER |= 0b0000000000010000;
+	// Init user button (I 11)
+	GPIOI->MODER |= 0b0000100000000000; /* configure PI11 in input mode */
+}
+
+int main(void) {
+	HAL_Init();
+	init_pins();
+
+	uint32_t first_led = (1 << 10);
+	uint32_t second_led = (1 << 9);
+	uint32_t third_led = (1 << 8);
+	uint32_t fourth_led = (1 << 7);
+	uint32_t user_button = (1 << 11);
+	uint32_t plus_button = (1 << 4);
+	//  uint32_t user_led = (1 << 10) | (1 << 9) | (1 << 8) | (1 << 7);
+
+	while (1) {
+		/* when button is pushed LED turns on, when released, then LED turns off */
+		if (GPIOI->IDR & (user_button)) {
+			/* set PI1 */
+			GPIOF->BSRR = first_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = first_led << 16;
+			GPIOF->BSRR = second_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = second_led << 16;
+			GPIOF->BSRR = third_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = third_led << 16;
+			GPIOF->BSRR = fourth_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = fourth_led << 16;
+		}
+		if (GPIOB->IDR & (plus_button)) {
+			GPIOF->BSRR = fourth_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = fourth_led << 16;
+			GPIOF->BSRR = third_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = third_led << 16;
+			GPIOF->BSRR = second_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = second_led << 16;
+			GPIOF->BSRR = first_led;
+			HAL_Delay(50);
+			GPIOF->BSRR = first_led << 16;
+		} else {
+			/* reset PI1 */
+
+			GPIOF->BSRR = first_led << 16;
+			GPIOF->BSRR = second_led << 16;
+			GPIOF->BSRR = third_led << 16;
+			GPIOF->BSRR = fourth_led << 16;
+			//  GPIOF->BSRR = user_led << 16;
+			/* we could use the ODR register also
+			 reset PI1, leave the other bits as they are
+			 GPIOI->ODR ^= green_led;
+			 */
+		}
+	}
+}
